d3079f6c0f47a0e062361b781f4a189950e8fbff
HARI KAUSHIK DAMU <155235456+Harikaushik134@users.noreply.github.com>
2026-09-18 16:19:14 +0530

    Add support for modulo, floor division, and exponentiationh

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 num1 = float(input("Enter first number: "))
-op = input("Enter operator (+, -, *, /): ")
+op = input("Enter operator (+, -, *, / , % , // , **):")
 num2 = float(input("Enter second number: "))
 
 if op == "+":
@@ -10,6 +10,12 @@ elif op == "*":
     result = num1 * num2
 elif op == "/":
     result = num1 / num2
+elif op == "%":
+    result = num1 % num2
+elif op == "//":
+    result = num1 // num2
+elif op == "**":
+    result = num1 ** num2
 else:
     result = "Invalid operator"
 
